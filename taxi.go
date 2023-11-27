bac549818aa3f60739d9e44e53b4398745a38cf8
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-11-27 12:37:41 +0300

    adding mor examples

--- taxi.go
+++ taxi.go
@@ -1,1 +1,29 @@
 package taxi
+
+type ITaxi interface {
+	getPrice() int
+}
+
+type Taxi struct{}
+
+func (p *Taxi) getPrice() int {
+	return 50
+}
+
+type LongTaxiTrip struct {
+	taxi ITaxi
+}
+
+func (c *LongTaxiTrip) getPrice() int {
+	taxiPrice := c.taxi.getPrice()
+	return taxiPrice - 10
+}
+
+type ShortTaxiTrip struct {
+	taxi ITaxi
+}
+
+func (s ShortTaxiTrip) getPrice() int {
+	taxiPrice := s.taxi.getPrice()
+	return taxiPrice + 40
+}
